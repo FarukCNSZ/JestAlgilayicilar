e984736b79069b8629e9d8ef0f32737e643268db
Faruk Cansiz <farukcnsz@gmail.com>
2023-01-14 00:36:36 +0300

    init commit

---
+++ JestAlgilayicilar/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  JestAlgilayicilar
+//
+//  Created by Faruk CANSIZ on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var ankara = true
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gorselDegistir))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+        
+    }
+    
+    @objc func gorselDegistir() {
+        
+        if ankara == false {
+            imageView.image = UIImage(named: "ankara")
+            label.text = "Ankara"
+            ankara = true
+        } else {
+            imageView.image = UIImage(named: "istanbul")
+            label.text = "İstanbul"
+            ankara = false
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
+
